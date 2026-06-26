6662d15aa9e752018481fcf43ce4f616652e5a3e
Win <win.sinor@gmail.com>
2026-06-26 14:17:22 -0400

    Comment out unused demo imports
    Comment out DrawDemo and PlinkoDemo imports.

--- display/demos/__init__.py
+++ display/demos/__init__.py
@@ -1,4 +1,4 @@
-from display.demos.draw import DrawDemo
+#from display.demos.draw import DrawDemo
 from display.demos.boids import BoidsDemo
 from display.demos.maze import MazeDemo
 from display.demos.fractal import FractalDemo
@@ -6,7 +6,7 @@ from display.demos.dvd import DvdDemo
 from display.demos.snake import SnakeDemo
 from display.demos.life import LifeDemo
 from display.demos.nbody import NBodyDemo
-from display.demos.plinko import PlinkoDemo
+#from display.demos.plinko import PlinkoDemo
 
 # Order here is the cycle order shown on screen, and the first entry is what
 # comes up on boot. To add a new demo: write a display/demos/<name>.py module
